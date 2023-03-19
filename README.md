95c497278ce5b4efe1e6243c8c68253030415d44
GabrielAzO <gabriel.a.oliveira0@hotmail.com>
2023-03-19 07:37:14 -0300

    primeiros commits

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Projeto de dados Uninove
 
 ## 💻 Pré-requisitos
 
-* Python v3.11 
+* <a href= "https://www.python.org/downloads/" Python v3.11> 
 
 ### Bibliotecas usadas
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#using https://www.kaggle.com/datasets/imdevskp/corona-virus-report
+# using https://www.kaggle.com/datasets/imdevskp/corona-virus-report
 
 import pandas as pnds
 import seaborn as sns
@@ -6,9 +6,45 @@ import matplotlib.pyplot as plt
 
 data = pnds.read_csv("4. covid_19_data.csv")
 
+# decidir heatmap
+
+
 # sns.heatmap(data.isnull())
 
 # plt.show()
 
 
+
+# mostrar os casos confirmados e recuperados de cada região/país 
+
 print (data.groupby("Region")['Confirmed', 'Recovered'].sum())
+
+#--------------------------------------------#
+
+
+# Remover todos os casos em que os confirmados são <10
+
+print (data = data[~(data.Confirmed < 10)])
+
+
+# Saber qual região temos o maior número de casos, organizar de forma ascendente o top 20
+
+print (data.groupby('Region').Confirmed.sum().sort_values(ascending = False).head(20))
+
+
+# Menor número de mortes, top 50
+
+print (data.groupby('Region').Deaths.sum().sort_values(ascending = True).head(50))
+
+
+# Quantos casos reportados no Brasil até 29/04/2020? 
+# US temos por estado
+
+print (data[data.Region == 'Brazil'])
+
+print (data[data.Region == 'US'])
+
+
+# Maior número de casos confirmados forma ascendente
+
+print (data.sort_values( by = ['Confirmed'] , ascending = True).head(50))
